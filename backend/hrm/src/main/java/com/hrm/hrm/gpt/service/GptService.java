ec3b6a0c1ddcb2bbcba6d27534a5f7b467d3133d
허진우 <jinu0806@korea.ac.kr>
2025-08-25 11:36:51 +0900

    AI1

--- backend/hrm/src/main/java/com/hrm/hrm/gpt/service/GptService.java
+++
@@ -1,8 +0,0 @@
-package com.hrm.hrm.gpt.service;
-
-import com.hrm.hrm.auth.dto.GptRequest;
-import com.hrm.hrm.auth.dto.GptResponse;
-
-public interface GptService {
-    GptResponse prompt(GptRequest request);
-} 
